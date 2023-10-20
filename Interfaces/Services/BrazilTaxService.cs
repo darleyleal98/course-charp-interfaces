e2c33a9f6d22872a43913586deb715fd56542797
Darley Leal <darleyleal98@gmail.com>
2023-10-20 04:50:49 -0300

    Adicionado as novas classes

---
+++ Interfaces/Services/BrazilTaxService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Services
+{
+    internal class BrazilTaxService
+    {
+        public double Tax(double amount)
+        {
+            return (amount <= 100.00) ? amount * 0.2 : amount * 0.15;
+        }
+    }
+}
